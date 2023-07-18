c33d044d4ed9320e329c5c38d359853c17e28898
David Poole <dbpoole7@gmail.com>
2023-07-18 18:49:17 -0500

    all code functioning except removal of all books for a single author upon that author's deletion

--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useRouter } from 'next/router';
 // import Image from 'next/image';
+import { useRouter } from 'next/router';
 import React, { useState, useEffect } from 'react';
 import BookCard from '../../components/BookCard';
 // import AuthorCard from '../../components/AuthorCard';
